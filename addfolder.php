dc19ce33db2532335781214258ab631564892c6d
tanny411 <aysha.kamal7@gmail.com>
2018-06-10 16:58:36 +0600

    unique folder name, no matter the path

--- addfolder.php
+++ addfolder.php
@@ -8,7 +8,7 @@
     $parent = mysqli_real_escape_string($con,trim($_POST['parent']));
     $groupid = mysqli_real_escape_string($con,$_POST['groupid']);
 
-    $query="select * from folder where group_id = ".$groupid." and folder='".$folder."' and parent='".$parent."'";
+    $query="select * from folder where group_id = ".$groupid." and folder='".$folder."'";
     if(!$query_run=mysqli_query($con,$query)) die('Server error');
     $rows=mysqli_num_rows($query_run);
     if($rows>0) die('<strong>Folder exists</strong>');
